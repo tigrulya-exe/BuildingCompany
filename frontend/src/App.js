013ce484587c20c407d231a3a23b1c07591b6b47
Tigran Manasyan <manasyan_tigran@mail.ru>
2020-05-04 20:40:30 +0700

    test front was implemented

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
+import CustomersTable from './components/CustomersTable'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Special React
-        </a>
-      </header>
+    <div>
+    <CustomersTable/>
+      Hello, worlds!
     </div>
   );
 }
