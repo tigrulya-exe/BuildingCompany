5f00d3bf251e7494f35af41f7c63e51d226aa5b3
Tigran Manasyan <manasyan_tigran@mail.ru>
2020-05-04 18:24:31 +0700

    data-rest was deleted

--- backend/src/main/kotlin/nsu/manasyan/buildingcompany/repositories/CustomersRepository.kt
+++ backend/src/main/kotlin/nsu/manasyan/buildingcompany/repositories/CustomersRepository.kt
@@ -2,9 +2,7 @@ package nsu.manasyan.buildingcompany.repositories
 
 import nsu.manasyan.buildingcompany.model.Customer
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.rest.core.annotation.RepositoryRestResource
 import org.springframework.stereotype.Repository
 
 @Repository
-@RepositoryRestResource(collectionResourceRel = "customers", path = "customers")
 interface CustomersRepository : JpaRepository<Customer, Int>
